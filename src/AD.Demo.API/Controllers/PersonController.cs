4e4a90df575459978392cc5866f54d7b93dcbff0
mattuu <matt@codemagix.co.uk>
2020-05-19 14:28:52 +0100

    Progress on PersonController

--- src/AD.Demo.API/Controllers/PersonController.cs
+++ src/AD.Demo.API/Controllers/PersonController.cs
@@ -28,16 +28,65 @@ namespace AD.Demo.API.Controllers
             return _context.People.ToList();
         }
 
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var entity = _context.People.Find(id);
+
+            if (entity != null)
+            {
+                return Ok(entity);
+            }
+
+            return NotFound();
+        }
+        
         [HttpPost]
         public IActionResult Post(People people)
         {
-            if(_context.People.Find(people.PersonId) != null){
+            if (_context.People.Find(people.PersonId) != null)
+            {
                 return Conflict();
             }
 
             _context.Add(people);
             _context.SaveChanges();
             return new StatusCodeResult(201);
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, People people)
+        {
+            var entity = _context.People.Find(id);
+
+            if (entity == null)
+            {
+                return BadRequest();
+            }
+
+            entity.FirstName = people.FirstName;
+            entity.LastName = people.LastName;
+            entity.IsAuthorised = people.IsAuthorised;
+            entity.IsEnabled = people.IsEnabled;
+            entity.IsValid = people.IsValid;
+
+            _context.SaveChanges();
+            return Ok(people);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var entity = _context.People.Find(id);
+
+            if (entity == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
